99ac75b51901fff19fc90f85016665dc13641c37
AnkitSinha <ankitsinha25061909@gmail.com>
2024-11-11 17:40:08 +0530

    Navigate from HomePage to DetailsPage easily

---
+++ Client/src/screens/HomeScreen.js
@@ -0,0 +1,30 @@
+// src/screens/HomeScreen.js
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details', { itemId: 86, otherParam: 'Some data' })}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+});
+
+export default HomeScreen;
